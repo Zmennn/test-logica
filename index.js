8890b18d666f6764bdae3e35e7ac8ac19f184365
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-12-10 14:50:42 +0200

    0,5

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const ERROR1 = 'Помилковий текст задачі. Речення мають бути розділенні одним із знаків "!?.".  Використовуйте ці знаки виключно у кінці речення. Також задача не може мати більше двох питань.'
 const ERROR2 = "Перше речення умови-це вхідні данні, обов'язково має включати в себе принаймні одне число, якщо на початку немає предметів використайте у реченні щось накшталт \"0 предметів\" "
-const ERROR3 = "Друге речення задачі описує дію і обов'язково має тільки одну дію, дії можуть бути такими \"з'їв, забрав, поклав\".";
+const ERROR3 = "Друге речення задачі описує дію і обов'язково має тільки одну дію, дії можуть бути такими \"з'їв,  поклав\".";
 const ERROR4 = "Друге речення задачі описує дію і обов'язково має в собі мінімум одне число.";
+const ERROR5 = "Третє та подальші речення задачі це запитання, обов'язково починаються зі слова \"Скільки\". Також перевірте крапки між попередніми реченнями.";
+const ERROR6 = "Кожне речення запитання має включити в себе тільки один об'єкт це \"хлопчик\" або \"столі\"."
+
 
 const FRUITS = ["яблука", "груші", "апельсини", "мандарини"];
-const ACTIONS = ["з'їв", "забрав", "поклав"];
+const ACTIONS = ["з'їв", "поклав"];
+const TARGET = ["столі", "хлопчик"];
+const DO = ["залишилось", "з'їв", "поклав"];
+const THINKS = [...FRUITS, "фрукти"];
 
 const startButton = document.querySelector('.form--button');
 const textField = document.querySelector('.form--text');
 
+let errorState = false;
+let startText = "";
+
 
 function onClickSubmit(ev) {
     ev.preventDefault();
-    const startText = textField.value.trim();
-    if (startText === "") {
-        textField.placeholder = "Введіть текст задачі";
-        textField.value = "";
-    } else {
 
+    if (!errorState) {
+        startText = textField.value.trim();
+        if (startText === "") {
+            textField.placeholder = "Введіть текст задачі";
+            textField.value = "";
+        } else {
+
+            textField.placeholder = "";
+            textField.value = "";
+            parse(startText);
+            // parse(" 3 апельсини 5 яблук,10 мандарин та 4 олівці лежали на столі. Хлопчик з'їв 4 мандарини та 2 апельсини. Скільки всього фруктів з'їв хлопчик? Скільки залишилось мандарин на столі?")
+        };
+    } else {
+        console.log(startText);
         textField.placeholder = "";
-        textField.value = "";
-        parse(startText);
-        // parse(" 3 апельсини 5 яблук,10 мандарин та 4 олівці лежали на столі. Хлопчик з'їв 4 мандарини та 2 апельсини. Скільки всього фруктів з'їв хлопчик? Скільки залишилось мандарин на столі?")
-    };
+        errorState = false;
+        startButton.innerText = "Відправити";
+        startButton.style.background = 'darkorange';
+        textField.value = startText;
+    }
 };
 
 function parse(startText) {
@@ -30,17 +49,17 @@ function parse(startText) {
     const regexp = /[^.!?]+[.!?]/igs;
     const textArray = startText.match(regexp);
     if (!textArray) {
-        textField.placeholder = ERROR1;
+        errorHandler(ERROR1);
         return
     };
     if (textArray.length < 3 || textArray.length > 5) {
-        textField.placeholder = ERROR1;
+        errorHandler(ERROR1);
         return
     };
 
     const initData = initDataHandler(transformToArray(textArray[0]));
     if (initData.length < 1) {
-        textField.placeholder = ERROR2;
+        errorHandler(ERROR2);
         return
     };
 
@@ -49,24 +68,51 @@ function parse(startText) {
 
     const action = actionHandler(transformToArray(textArray[1]));
     if (action.length !== 1) {
-        textField.placeholder = ERROR3;
+        errorHandler(ERROR3);
     }
     console.log(action);
 
     const actionData = initDataHandler(transformToArray(textArray[1]));
     if (actionData.length < 1) {
-        textField.placeholder = ERROR4;
+        errorHandler(ERROR4);
         return
     };
 
     const normalizedActionData = normalize(actionData)
     console.log(normalizedActionData);
+
+    for (let index = 2; index < textArray.length; index++) {
+        let targetArray = [];
+        const wordsArray = transformToArray(textArray[index].trim(''))
+        if (wordsArray[0] !== "Скільки") {
+            errorHandler(ERROR5);
+            return
+        };
+
+
+        TARGET.forEach((element) => {
+            const regexp = createRegexp(element);
+
+            wordsArray.forEach((word) => {
+                if (regexp.test(word)) { targetArray.push(element) }
+            })
+        })
+        if (targetArray.length !== 1) {
+            errorHandler(ERROR6);
+            return
+        }
+        console.log(targetArray);
+    }
 };
+
+
+
+
 
 function normalize(initData) {
     let newData = {};
     FRUITS.forEach((element) => {
-        const regexp = new RegExp(element.slice(0, element.length - 2), "i");
+        const regexp = createRegexp(element);
 
         initData.forEach((oldElement) => {
             if (regexp.test(oldElement[1])) {
@@ -75,6 +121,10 @@ function normalize(initData) {
         })
     });
     return newData;
+};
+
+function createRegexp(element) {
+    return new RegExp(element.slice(0, element.length - 2), "i")
 };
 
 function transformToArray(text) {
@@ -98,10 +148,17 @@ function actionHandler(textArray) {
     ACTIONS.forEach((element => {
         const wordsArray = (textArray.filter((el) => el === element));
         if (wordsArray.length > 1) {
-            textField.placeholder = ERROR3;
+            errorHandler(ERROR3);
         } else if (wordsArray[0]) { action.push(wordsArray[0]) };
     }))
     return action
+};
+
+function errorHandler(err) {
+    textField.placeholder = err;
+    errorState = true;
+    startButton.innerText = "Повернутись";
+    startButton.style.background = 'red';
 }
 
 
