1aa3fb72a477576b8c71fe25646930a6a5c70c8d
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-12-12 22:58:47 +0200

    1.01 fix

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ function parse(startText) {
     };
 
     const initData = initDataHandler(transformToArray(textArray[0]));
+    console.log(initData);
     if (initData.length < 1) {
         errorHandler(ERROR2);
         return
@@ -141,16 +142,17 @@ function processingData(initData, action, actionData, allQuestions) {
 
     if (action[0] === "з'їв") {
         for (const key in actionData) {
-            middlObject[key] = Number(initData[key]) - Number(actionData[key])
+            middlObject[key] = Number(initData[key]) ? Number(initData[key]) : 0 - Number(actionData[key])
         };
         const table = { ...initData, ...middlObject };
         addAll(table);
+        console.log(table);
 
         genereyteAnsver(table, actionData, allQuestions)
 
     } else if (action[0] === "поклав") {
         for (const key in actionData) {
-            middlObject[key] = Number(initData[key]) + Number(actionData[key])
+            middlObject[key] = Number(initData[key]) ? Number(initData[key]) : 0 + Number(actionData[key])
         };
         const table = { ...initData, ...middlObject };
 
